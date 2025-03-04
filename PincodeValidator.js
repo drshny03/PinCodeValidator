05a831ba03fa5a65b9bd65b3c16a9ec11b9870f5
drshny03 <darshanyadav7049@gmail.com>
2025-03-04 12:35:02 +0530

    [Darshan] Add . Restrict the PIN code from taking alphabets or special characters at the beginning.

--- PincodeValidator.js
+++ PincodeValidator.js
@@ -1,5 +1,5 @@
 //regex to validate pincode
-const regex = /^[0-9]{6}/;
+const regex = /^[0-9][0-9]{5}$/;
 
 //function to validate pin code
 function validate(pincode){
@@ -7,3 +7,4 @@ function validate(pincode){
 }
 
 console.log(validate(400088));
+console.log(validate("A40088"));
